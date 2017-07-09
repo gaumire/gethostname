89c6b3098c2c2c3ff914d2e412168da9925febed
Gaurav Ghimire <gaurav.ghimire@gmail.com>
2017-07-10 08:51:28 +1000

    added secrets

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 import socket
 import simplejson as json
 import uuid
+
 app = Flask(__name__)
 
 
@@ -25,3 +26,23 @@ def getuuid():
     data['hostname'] = hostname
     data['uuid'] = uuid_data
     return json.dumps(data)
+
+
+@app.route('/secret')
+def getsecret():
+    """Return secrts if any & uuid."""
+    data = {}
+    hostname = socket.gethostname()
+    try:
+        secret = open('/run/secrets/mysecret').read()
+        if secret is not None:
+            data['hostname'] = hostname
+            data['secret'] = secret
+            return json.dumps(data)
+
+        else:
+            data['error'] = 'No secret available!'
+            return json.dumps(data)
+    except Exception as e:
+        data['error'] = e
+        return json.dumps(data)
